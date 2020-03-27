cd7c057529924a383a18ee5e12e7c909270c21fe
Cory Moore <moore11b@gmail.com>
2020-03-27 14:26:53 -0400

    Dot Net Migration/Update
    This branch was a push from and outdated dotnet module.  It was converted to VS 2019.
    Note: Use " EntityFrameworkCore/Update-Database  " to update database.

---
+++ Padua/Data/Migrations/20200327181651_testMig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LabW11Authentication.Data.Migrations
+{
+    public partial class testMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Padua/Data/Migrations/20200327181745_testMig1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LabW11Authentication.Data.Migrations
+{
+    public partial class testMig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
